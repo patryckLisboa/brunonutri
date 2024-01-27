4859ec73471950a57359e916c03aadf9a8f00d5d
patryckLisboa <patryck-lisboa@hotmail.com>
2024-01-27 00:03:08 -0300

    protótipo do e-book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { EBookComponent } from './components/e-book/e-book.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'ebook', component: EBookComponent },
+
 ];
 
 @NgModule({
---
+++ src/app/components/e-book/e-book.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-e-book',
+  standalone: true,
+  imports: [],
+  templateUrl: './e-book.component.html',
+  styleUrl: './e-book.component.scss'
+})
+export class EBookComponent {
+
+}
