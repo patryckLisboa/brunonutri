87a67f770e0528df86fe79c17dee52e81d8ef4c3
patryckLisboa <patryck-lisboa@hotmail.com>
2024-02-06 13:54:51 -0300

    ajustes estéticos gerais

--- src/app/shared/components/p-carousel/p-carousel.component.ts
+++ src/app/shared/components/p-carousel/p-carousel.component.ts
@@ -21,17 +21,17 @@ export class PCarouselComponent {
     stretch: 0,
     depth: window.innerWidth < 750 ? 300 : 0,
     modifier: 1,
-    slideShadows: true
-  }
+    slideShadows: true,
+  };
 
   ngAfterViewInit() {
-    setTimeout(() =>  this.loadingPage = false, 0);
+    setTimeout(() => (this.loadingPage = false), 0);
   }
 
   toggleImageModal(index: any = null): any {
-    if(index || index == 0){
+    if (index || index == 0) {
       this.imgModalIndex = index;
-      return this.imagens[index].expanded = true;
+      return (this.imagens[index].expanded = true);
     }
     this.imagens[this.imgModalIndex].expanded = false;
   }
@@ -41,154 +41,139 @@ export class PCarouselComponent {
       index: 1,
       src: '../../../../assets/img/img-evolutions/img (1).jpeg',
       description: '6 meses',
-      expanded: false
+      expanded: false,
     },
     {
       index: 2,
       src: '../../../../assets/img/img-evolutions/img (2).jpeg',
       description: '2 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 3,
       src: '../../../../assets/img/img-evolutions/img (3).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 4,
       src: '../../../../assets/img/img-evolutions/img (4).jpeg',
       description: '1 mês',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 5,
       src: '../../../../assets/img/img-evolutions/img (5).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 6,
       src: '../../../../assets/img/img-evolutions/img (6).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 7,
       src: '../../../../assets/img/img-evolutions/img (7).jpeg',
       description: '2 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 8,
       src: '../../../../assets/img/img-evolutions/img (8).jpeg',
       description: '2 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 9,
       src: '../../../../assets/img/img-evolutions/img (9).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 10,
       src: '../../../../assets/img/img-evolutions/img (10).jpeg',
       description: '4 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 11,
       src: '../../../../assets/img/img-evolutions/img (11).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 12,
       src: '../../../../assets/img/img-evolutions/img (12).jpeg',
       description: '1 mês',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 13,
       src: '../../../../assets/img/img-evolutions/img (13).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 14,
       src: '../../../../assets/img/img-evolutions/img (14).jpeg',
       description: '5 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 15,
       src: '../../../../assets/img/img-evolutions/img (15).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 16,
       src: '../../../../assets/img/img-evolutions/img (16).jpeg',
       description: '3 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 17,
       src: '../../../../assets/img/img-evolutions/img (17).jpeg',
       description: '5 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 18,
       src: '../../../../assets/img/img-evolutions/img (18).jpeg',
       description: '1 ano',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 19,
       src: '../../../../assets/img/img-evolutions/img (19).jpeg',
       description: '1 mês',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 20,
       src: '../../../../assets/img/img-evolutions/img (20).jpeg',
       description: '4 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 21,
       src: '../../../../assets/img/img-evolutions/img (21).jpeg',
       description: '5 meses',
-      expanded: false
-
+      expanded: false,
     },
     {
       index: 22,
       src: '../../../../assets/img/img-evolutions/img (22).jpeg',
       description: '8 meses',
-      expanded: false
-
+      expanded: false,
+    },
+    {
+      index: 23,
+      src: '../../../../assets/img/img-evolutions/img (23).jpeg',
+      description: '4 meses',
+      expanded: false,
     },
   ];
 
@@ -197,6 +182,6 @@ export class PCarouselComponent {
   }
 
   onSlideChange(event: any) {
-    this.imgIndex =  event[0].activeIndex ;
+    this.imgIndex = event[0].activeIndex;
   }
 }
